1f6c0c9dc5f3bec43b0c73bcc206526d84b97919
brentmc <brentmcivor.mail+workgithub@gmail.com>
2017-02-11 18:23:33 +1000

    Added WebpackDashboard and split webpackconfig based on target

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
-var webpack = require('webpack')
+const merge = require('webpack-merge');
+const webpack = require('webpack')
+const DashboardPlugin = require('webpack-dashboard/plugin');
 
-module.exports = {
+const env = process.env
+
+const TARGET = env.npm_lifecycle_event;
+env.BABEL_ENV = TARGET;
+
+// NODE_ENV set to production will remove all React
+// prop validations.
+// Because we want to simulate a prod build in staging,
+// we needed another ENVIRONMENT variable.
+require('dotenv').config({
+  path: './configs/.env.' + env.ENVIRONMENT
+});
+
+
+const common = {
   entry: './index.js',
 
   output: {
@@ -8,18 +24,32 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/'
   },
-
-  // add this handful of plugins that optimize the build
-  // when we're in production
-  plugins: process.env.NODE_ENV === 'production' ? [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin()
-  ] : [],
+  
+  plugins: [],
 
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
     ]
   }
+};
+
+if (TARGET === 'watch') {
+  module.exports = merge(common, {
+    // Add these plugins for dev watch
+    plugins: [
+      new DashboardPlugin(),
+    ]
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {
+    // Add this handful of plugins that optimize the build when we're in production
+    plugins: [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin()
+    ]
+  });
 }
